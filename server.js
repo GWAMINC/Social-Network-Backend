2892ac901c19bf94e375b3053512d2cd1441bb7b
jukistricker <thanhthanhbg1@gmail.com>
2024-08-01 02:45:42 +0700

    reup comment model and crud

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import connectDB from "./utils/db.js"
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
-import commentRoutes from "./routes/comment.route.js";
+
 dotenv.config({});
 
 const app = express();
