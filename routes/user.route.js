7ff666524d8585e480b6d478f759467fa482d7b1
truongpa <truongvunu@gmail.com>
2024-09-15 15:48:57 +0700

    profile

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,12 +6,12 @@ import {
   updateProfile,
   getProfile,
   followUser,
-  getUser,
   unfollowUser,
   addFriend,
   getFriends,
   deleteFriend,
   getAllusers,
+  getProfileById,
 } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 
@@ -20,13 +20,14 @@ const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").post(isAuthenticated, logout);
-router.route("/getallusers").post(isAuthenticated,getAllusers);
-router.route("/getUser/:id").get(isAuthenticated, getUser);
+router.route("/getallusers").post(isAuthenticated, getAllusers);
+// router.route("/getUser/:id").get(isAuthenticated, getUser);
 router.route("/profile").get(isAuthenticated, getProfile);
+router.route("/getProfileById").post(isAuthenticated, getProfileById);
 router.route("/profile/update").post(isAuthenticated, updateProfile);
 router.route("/followUser").post(isAuthenticated, followUser);
 router.route("/unfollowUser").post(isAuthenticated, unfollowUser);
-router.route("/addfriend").post(isAuthenticated, addFriend);
+router.route("/addfriend").post(isAuthenticated, addFriend); 
 router.route("/unfriend").post(isAuthenticated, deleteFriend);
 router.route("/getfriend").post(isAuthenticated, getFriends);
 export default router;
