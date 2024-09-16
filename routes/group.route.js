21e5c7501b3b5ce5f17463825d0bca9e4ab127ba
qhxiv <eknight1412@gmail.com>
2024-09-16 13:30:40 +0700

    add getUsersByName, getGroupsByName, getPostsByContent api for the search bar

--- routes/group.route.js
+++ routes/group.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import{createGroup,updateGroup,deleteGroup, getGroup, getGroupByUser, getGroupById} from "../controllers/group.controller.js";
+import{createGroup,updateGroup,deleteGroup, getGroup, getGroupByUser, getGroupById, getGroupsByName} from "../controllers/group.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { checkAdminGroup } from "../middlewares/checkAdminGroup.js";
 import { upload } from "../utils/cloudinary.js";
@@ -11,4 +11,5 @@ router.route("/getGroupByUser").get(isAuthenticated,getGroupByUser);
 router.route("/updateGroup").post(isAuthenticated,checkAdminGroup,updateGroup);
 router.route("/deleteGroup").post(isAuthenticated,checkAdminGroup,deleteGroup);
 router.route("/getGroupById").post(isAuthenticated,getGroupById);
+router.route("/getGroupsByName/:name").get(isAuthenticated, getGroupsByName);
 export default router;
